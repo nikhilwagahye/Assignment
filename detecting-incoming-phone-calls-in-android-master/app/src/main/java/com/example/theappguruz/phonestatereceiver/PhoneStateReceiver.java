55b8450c0567f52a592bb932aa0fcc40ca13b08f
Nikhilkumar Waghaye <nikhilkumar.waghaye@IN.valtech.com>
2018-05-13 01:42:37 +0530

    Outgoing call done

--- detecting-incoming-phone-calls-in-android-master/app/src/main/java/com/example/theappguruz/phonestatereceiver/PhoneStateReceiver.java
+++ detecting-incoming-phone-calls-in-android-master/app/src/main/java/com/example/theappguruz/phonestatereceiver/PhoneStateReceiver.java
@@ -22,52 +22,58 @@ public class PhoneStateReceiver extends BroadcastReceiver {
             SharedPreferences pref = context.getSharedPreferences("callapppref", 0);
             SharedPreferences.Editor editor = pref.edit();
 
-            System.out.println("Receiver start");
-            String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
-            final String incomingNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
-
-            if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
-                Toast.makeText(context, "Incoming Call State", Toast.LENGTH_SHORT).show();
-                Toast.makeText(context, "Ringing State Number is -" + incomingNumber, Toast.LENGTH_SHORT).show();
-
-                editor.putBoolean("call_received", false);
+            if (intent.getAction().equals("android.intent.action.NEW_OUTGOING_CALL")) {
+                Toast.makeText(context, "OUTGOING Call", Toast.LENGTH_SHORT).show();
+                editor.putBoolean("outgoing", true);
                 editor.commit();
-            }
-            if ((state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK))) {
-                Toast.makeText(context, "Call Received State", Toast.LENGTH_SHORT).show();
+            } else {
 
-                editor.putBoolean("call_received", true);
-                editor.commit();
+                System.out.println("Receiver start");
+                String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
+                final String incomingNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
 
+                if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
+//                    Toast.makeText(context, "Incoming Call State", Toast.LENGTH_SHORT).show();
+//                    Toast.makeText(context, "Ringing State Number is -" + incomingNumber, Toast.LENGTH_SHORT).show();
 
-            }
-            if (state.equals(TelephonyManager.EXTRA_STATE_IDLE)) {
-                Toast.makeText(context, "Call Idle State", Toast.LENGTH_SHORT).show();
-                /*Intent scheduledIntent = new Intent(context.getPackageName(), PopupActivity.class);
-                scheduledIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                scheduledIntent.addFlags(Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
-                context.startActivity(scheduledIntent);*/
+                    editor.putBoolean("call_received", false);
+                    editor.putBoolean("outgoing", false);
 
-                if (pref.getBoolean("call_received", false) == true) {
+                    editor.commit();
+                }
+                if ((state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK))) {
+                    //  Toast.makeText(context, "Call Received State", Toast.LENGTH_SHORT).show();
 
-                    // call received and disconnects
-                    new android.os.Handler().postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            Intent intentone = new Intent(context.getApplicationContext(), PopupActivity.class);
-                            intentone.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-                            intentone.putExtra("phoneNo", incomingNumber);
-                            context.startActivity(intentone);
-                        }
-                    }, 2000);
-                } else {
-
-                    // without receiving call, it disconnects
+                    editor.putBoolean("call_received", true);
+                    editor.commit();
 
 
                 }
+                if (state.equals(TelephonyManager.EXTRA_STATE_IDLE)) {
+                    //  Toast.makeText(context, "Call Idle State", Toast.LENGTH_SHORT).show();
+
+                    if (pref.getBoolean("call_received", false) == true &&
+                            pref.getBoolean("outgoing", false) == false) {
+
+                        // call received and disconnects
+                        new android.os.Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                Intent intentone = new Intent(context.getApplicationContext(), PopupActivity.class);
+                                intentone.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                intentone.putExtra("phoneNo", incomingNumber);
+                                context.startActivity(intentone);
+                            }
+                        }, 2000);
+                    } else {
+
+                        // without receiving call, it disconnects
 
 
+                    }
+
+
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
